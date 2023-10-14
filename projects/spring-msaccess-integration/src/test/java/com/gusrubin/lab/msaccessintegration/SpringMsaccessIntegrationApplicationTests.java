93ba72d18c2adf691da06ca5a1bf4f59cb3c2db9
Gustavo Rubin <gusrubin@gmail.com>
2023-10-14 18:01:43 -0300

    Adds spring-msaccess-integration

---
+++ projects/spring-msaccess-integration/src/test/java/com/gusrubin/lab/msaccessintegration/SpringMsaccessIntegrationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gusrubin.lab.msaccessintegration;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringMsaccessIntegrationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
