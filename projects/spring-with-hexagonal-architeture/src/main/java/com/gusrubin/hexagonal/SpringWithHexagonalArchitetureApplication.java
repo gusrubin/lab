67166a14d02c673af7fce3d09fe62dcbfdfd2c8c
Gustavo Rubin <gusrubin@gmail.com>
2025-03-19 14:21:48 -0300

    spring-with-hexagonal-architeture backbone

---
+++ projects/spring-with-hexagonal-architeture/src/main/java/com/gusrubin/hexagonal/SpringWithHexagonalArchitetureApplication.java
@@ -0,0 +1,13 @@
+package com.gusrubin.hexagonal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringWithHexagonalArchitetureApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringWithHexagonalArchitetureApplication.class, args);
+	}
+
+}
---
+++ projects/spring-with-hexagonal-architeture/src/test/java/com/gusrubin/hexagonal/SpringWithHexagonalArchitetureApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gusrubin.hexagonal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringWithHexagonalArchitetureApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
