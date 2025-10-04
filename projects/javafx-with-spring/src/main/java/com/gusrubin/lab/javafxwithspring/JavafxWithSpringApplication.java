178e0160ca1537684d209a568923f02ddf310d91
Gustavo Rubin <gusrubin@gmail.com>
2025-10-04 18:05:14 -0300

    javafx-with-spring updates

--- projects/javafx-with-spring/src/main/java/com/gusrubin/lab/javafxwithspring/JavafxWithSpringApplication.java
+++ projects/javafx-with-spring/src/main/java/com/gusrubin/lab/javafxwithspring/JavafxWithSpringApplication.java
@@ -1,15 +1,13 @@
 package com.gusrubin.lab.javafxwithspring;
 
-import org.springframework.boot.autoconfigure.SpringBootApplication;
 import com.gusrubin.lab.javafxwithspring.application.gui.JavaFxApplication;
-
 import javafx.application.Application;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class JavafxWithSpringApplication {
 
-    public static void main(String[] args) {
-	Application.launch(JavaFxApplication.class, args);
-    }
-
+  public static void main(String[] args) {
+    Application.launch(JavaFxApplication.class, args);
+  }
 }
--- projects/javafx-with-spring/src/main/java/com/gusrubin/lab/javafxwithspring/application/gui/JavaFxApplication.java
+++ projects/javafx-with-spring/src/main/java/com/gusrubin/lab/javafxwithspring/application/gui/JavaFxApplication.java
@@ -1,12 +1,8 @@
 package com.gusrubin.lab.javafxwithspring.application.gui;
 
+import com.gusrubin.lab.javafxwithspring.JavafxWithSpringApplication;
 import java.io.IOException;
 import java.net.URL;
-import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.context.ApplicationContextInitializer;
-import org.springframework.context.ConfigurableApplicationContext;
-import org.springframework.context.support.GenericApplicationContext;
-import com.gusrubin.lab.javafxwithspring.JavafxWithSpringApplication;
 import javafx.application.Application;
 import javafx.application.HostServices;
 import javafx.application.Platform;
@@ -17,92 +13,103 @@ import javafx.scene.image.Image;
 import javafx.stage.Stage;
 import javafx.stage.StageStyle;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.context.ApplicationContextInitializer;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.support.GenericApplicationContext;
 
 @Slf4j
 public class JavaFxApplication extends Application {
 
-    private ConfigurableApplicationContext springApplicationContext;
+  private ConfigurableApplicationContext springApplicationContext;
 
-    // This method is necessary when run by java -jar
-    public static void main(String[] args) {
-	launch(args);
+  // This method is necessary when run by java -jar
+  public static void main(String[] args) {
+    launch(args);
+  }
+
+  @Override
+  public void start(Stage primaryStage) throws Exception {
+    log.info("Starting application");
+    log.info("Show splash screen");
+    showSplashScreen(primaryStage);
+
+    new Thread(
+            () -> {
+              try {
+                Thread.sleep(500);
+
+                Platform.runLater(
+                    () -> {
+                      try {
+                        initSpringContext();
+
+                        GlobalExceptionHandler.setupGlobalExceptionHandler(primaryStage);
+
+                        log.info("Show main window");
+                        springApplicationContext.publishEvent(
+                            new PrimaryStageReadyEvent(primaryStage));
+
+                        log.info("Close splash screen");
+                        primaryStage.close();
+
+                      } catch (Exception e) {
+                        log.error("Failed to load application. {}", e.getMessage());
+                      }
+                    });
+
+              } catch (InterruptedException e) {
+                log.error("Failed to load application. {}", e.getMessage());
+                Thread.currentThread().interrupt();
+              }
+            })
+        .start();
+  }
+
+  private void showSplashScreen(Stage primaryStage) {
+    Parent root = null;
+
+    try {
+      URL fxmlURL = getClass().getResource("/views/SplashScreenView.fxml");
+
+      root = FXMLLoader.load(fxmlURL);
+
+      if (fxmlURL == null) {
+        throw new IOException("SplashScreenView.fxml not found");
+      }
+
+    } catch (IOException e) {
+      e.printStackTrace();
     }
 
-    @Override
-    public void start(Stage primaryStage) throws Exception {
-	log.info("Starting application");
-	log.info("Show splash screen");
-	showSplashScreen(primaryStage);
+    primaryStage.setTitle("JavaFX Hello World");
+    Image icon = new Image(getClass().getResourceAsStream("/icon.png"));
+    primaryStage.getIcons().addAll(icon);
+    primaryStage.alwaysOnTopProperty();
+    primaryStage.centerOnScreen();
+    primaryStage.initStyle(StageStyle.UNDECORATED);
+    primaryStage.setScene(new Scene(root));
+    primaryStage.show();
+  }
 
-	new Thread(() -> {
-	    try {
-		Thread.sleep(500);
+  private void initSpringContext() {
+    ApplicationContextInitializer<GenericApplicationContext> initializer =
+        applicationContext -> {
+          applicationContext.registerBean(Application.class, () -> JavaFxApplication.this);
+          applicationContext.registerBean(Parameters.class, this::getParameters);
+          applicationContext.registerBean(HostServices.class, this::getHostServices);
+        };
+    this.springApplicationContext =
+        new SpringApplicationBuilder()
+            .sources(JavafxWithSpringApplication.class)
+            .initializers(initializer)
+            .run(getParameters().getRaw().toArray(new String[0]));
+  }
 
-		Platform.runLater(() -> {
-		    try {
-			initSpringContext();
-
-			GlobalExceptionHandler.setupGlobalExceptionHandler(primaryStage);
-
-			log.info("Show main window");
-			springApplicationContext.publishEvent(new PrimaryStageReadyEvent(primaryStage));
-
-			log.info("Close splash screen");
-			primaryStage.close();
-
-		    } catch (Exception e) {
-			log.error("Failed to load application. {}", e.getMessage());
-		    }
-		});
-
-	    } catch (InterruptedException e) {
-		log.error("Failed to load application. {}", e.getMessage());
-		Thread.currentThread().interrupt();
-	    }
-	}).start();
-    }
-
-    private void showSplashScreen(Stage primaryStage) {
-	Parent root = null;
-
-	try {
-	    URL fxmlURL = getClass().getResource("/views/SplashScreenView.fxml");
-
-	    root = FXMLLoader.load(fxmlURL);
-
-	    if (fxmlURL == null) {
-		throw new IOException("SplashScreenView.fxml not found");
-	    }
-
-	} catch (IOException e) {
-	    e.printStackTrace();
-	}
-
-	primaryStage.setTitle("JavaFX Hello World");
-	Image icon = new Image(getClass().getResourceAsStream("/icon.png"));
-	primaryStage.getIcons().addAll(icon);
-	primaryStage.alwaysOnTopProperty();
-	primaryStage.centerOnScreen();
-	primaryStage.initStyle(StageStyle.UNDECORATED);
-	primaryStage.setScene(new Scene(root));
-	primaryStage.show();
-    }
-
-    private void initSpringContext() {
-	ApplicationContextInitializer<GenericApplicationContext> initializer = applicationContext -> {
-	    applicationContext.registerBean(Application.class, () -> JavaFxApplication.this);
-	    applicationContext.registerBean(Parameters.class, this::getParameters);
-	    applicationContext.registerBean(HostServices.class, this::getHostServices);
-	};
-	this.springApplicationContext = new SpringApplicationBuilder().sources(JavafxWithSpringApplication.class)
-		.initializers(initializer).run(getParameters().getRaw().toArray(new String[0]));
-    }
-
-    @Override
-    public void stop() throws Exception {
-	log.info("Application closed");
-	this.springApplicationContext.close();
-	Platform.exit();
-    }
-
+  @Override
+  public void stop() throws Exception {
+    log.info("Application closed");
+    this.springApplicationContext.close();
+    Platform.exit();
+  }
 }
